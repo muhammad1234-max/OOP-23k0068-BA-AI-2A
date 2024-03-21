733c38348b18ec3b62b71ee134c56b6b4b4829c8
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2024-03-22 02:09:20 +0500

    Create q1.cpp

---
+++ lab8/q1.cpp
@@ -0,0 +1,61 @@
+/*
+Name: Muhammad Abbas
+ID:23K-0068
+Purpose: code to show simple inheritance between book and fiction book class
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//base class 
+class Book {
+	protected:
+    	string title;
+    	string author;
+    	string publisher;
+
+	public:
+    	//parameterized constructor with initializer list
+    Book(string atitle, string aauthor, string apublisher)
+        : title(atitle), author(aauthor), publisher(apublisher) {}
+
+    //display function
+    void display() const {
+    	cout<<"information about the book"<<endl;
+        cout<<"Title: "<<title<<endl;
+        cout<<"Author: "<<author << endl;
+        cout<<"Publisher: "<<publisher<<endl;
+    }
+};
+
+//derived or subclass 
+class FictionBook : public Book {
+	private:
+    	string genre;
+    	string protagonist;
+
+	public:
+    	//parameterized constructor with initializer list for inheritance
+    	FictionBook(string atitle, string aauthor, string apublisher, string agenre, string aprotagonist)
+        : Book(atitle, aauthor, apublisher), genre(agenre), protagonist(aprotagonist) {}
+
+    //second implemenyain of display function (function overriding)
+    void display() const {
+        //get existing implementation of the display
+        Book::display();
+        //define additional implementation
+        cout<<"Genre: "<<genre<<endl;
+        cout<<"Protagonist: "<<protagonist<<endl;
+    }
+};
+
+int main() {
+	cout<<"Muhammad Abbas\n23k-0068"<<endl;
+    //instance of the derived class
+    FictionBook book("it ends with us", "Collen Hoover", "oxford", "Romantic-Fiction", "lily Bloom");
+    book.display();
+
+    return 0;
+}
